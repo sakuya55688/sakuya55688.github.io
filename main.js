a30ac2187882b328b39b07ab7d0832cf702858d4
sakuya55688 <jerry281123@gmail.com>
2022-02-13 22:26:55 +0800

    add gif

--- main.js
+++ main.js
@@ -1,10 +1,34 @@
+/* control sidebar */
+const vw = window.innerWidth;
+const navbar = document.querySelector(".nav-bar");
+const slider = document.querySelector(".swiper-wrapper");
 
-/* control slider*/
 let mousedown = false;
 let swipeDirection = 1;
 let startX, scrollLeft;
-const slider = document.querySelector(".swiper-wrapper");
 
+
+const toggleNav = (event) => {
+    if(event.target.id === "menu-icon"){
+        console.log(vw);
+        if(vw >= 900)
+            navbar.style.width = "30vw";
+        else
+            navbar.style.width = "50vw";
+        
+    }
+    else{
+        navbar.style.width = 0;
+        
+    }
+}
+
+
+
+
+
+
+/* control slider*/
 slider.addEventListener("mousedown", (e) => {
     mousedown = true;
     slider.classList.add("active");
@@ -15,7 +39,7 @@ slider.addEventListener("mousedown", (e) => {
 slider.addEventListener("mouseup", (e) => {
     mousedown = false;
     slider.classList.remove("active");
-    console.log("dot position:", (slider.scrollLeft / 440));
+    //console.log("dot position:", (slider.scrollLeft / 440));
     /* switch navigation dot*/
     
 });
@@ -31,6 +55,10 @@ slider.addEventListener("mousemove", (e) => {
     const scroll = (x - startX) * 3; //scroll faster
     slider.scrollLeft = scrollLeft - scroll;
 
-    console.log("scrollleft:", scroll);
+    //console.log("scrollleft:", scroll);
 });
+
+
+
+
 
